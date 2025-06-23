c0d4f54fde1aa35ecf86afc4eda05ecd77d691f0
sanmay321 <sanmaydas2002@gmail.com>
2025-06-23 23:45:51 +0530

    Removed secret

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -49,9 +49,3 @@ const titleSmall = TextStyle(
     fontSize: 10
 );
 
-// class StripeConfig {
-//   static const String publishableKey = 'pk_test_51Om3i3CcxmpT621JxYdM8wc6L5eaB7cdhkVp4fbZOYRuuYWqHKnEDKIOn44PglGZhAoa7tMYVAKqFFSSFeZecmCV00qP9fBFMO';
-//   static const String secretKey = 'sk_test_51Om3i3CcxmpT621JF9WnBQGWe4D3AvFv1y0Ks6CcFpNtxF6v81DivmT0jiKDn79OQ3VFGmt5Ulq9S8W92yZDdCE600n8B9xroR';
-//   static const String stripeUrl = 'https://api.stripe.com/v1';
-//   static const String stripeMerchantId = 'your_merchant_id'; // For Apple Pay
-// }
